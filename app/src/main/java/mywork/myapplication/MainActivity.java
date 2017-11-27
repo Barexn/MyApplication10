aca654f5112eb29d5dde6f6205bc6c3df445a889
yqq <2687698398@qq.com>
2017-11-27 19:53:54 +0800

    000000

--- app/src/main/java/mywork/myapplication/MainActivity.java
+++ app/src/main/java/mywork/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package mywork.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import mywork.mylibrary.Test;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //System.out.println("======"+Test.add(1,2));
     }
 }
---
+++ mylibrary/src/main/java/mywork/mylibrary/Test.java
@@ -0,0 +1,11 @@
+package mywork.mylibrary;
+
+/**
+ * Created by Bare on 2017/11/27.
+ */
+
+public class Test {
+    public static int add(int i,int j){
+        return i+j;
+    }
+}
